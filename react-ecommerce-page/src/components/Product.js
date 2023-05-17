6fb0bb94c442f88f879c5005d08a7b5ec7863c18
James Provance <jb24x7@gmail.com>
2023-05-17 16:02:56 -0700

    updated product cards to have price display at the bottom

--- react-ecommerce-page/src/components/Product.js
+++ react-ecommerce-page/src/components/Product.js
@@ -8,33 +8,29 @@ import {
 
 
 function Product(props) {
-
   return (
-
-    < React.Fragment >
-      <Card maxW='sm' onClick={() => props.whenProductClicked(props.id)}>
+    <React.Fragment>
+      <Card maxW='sm' onClick={() => props.whenProductClicked(props.id)} backgroundColor={"gray.100"}>
         <CardBody>
-          <Box
-            h="200px"
-            bgImage={`url(${props.imageUrl})`}
-            bgSize="cover"
-            bgPosition="center"
-          />
+          <Box h="200px" bgImage={`url(${props.imageUrl})`} bgSize="cover" bgPosition="center" />
           <Stack mt='2' spacing='3' textAlign="left">
             <Text>
-              {props.name.length > 25
-                ? `${props.name.substring(0, 25)}...`
-                : props.name}
-            </Text>
-            <Text fontWeight={"bold"} color='blue.600' fontSize='2xl'>
-              ${props.price}
+              {props.name.length > 25 ? `${props.name.substring(0, 25)}...` : props.name}
             </Text>
           </Stack>
         </CardBody>
+        <CardFooter>
+          <Stack spacing='3' textAlign="left">
+            <Text fontWeight={"bold"} fontSize='2xl'>
+              ${props.price}
+            </Text>
+          </Stack>
+        </CardFooter>
       </Card>
-    </React.Fragment >
+    </React.Fragment>
   );
 }
+
 
 Product.propTypes = {
   name: PropTypes.string,
